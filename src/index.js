5336033834b2ea55c24f43b92ec2e31c83e33f33
戚雨 <yuqi@wisedu.com>
2018-06-04 17:25:08 +0800

    项目初始化

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './app.vue'
+import iView from 'bh-iview';
+import 'bh-iview/dist/styles/iview.css';
+Vue.use(iView);
+import tgTuring from 'tg-turing';
+import 'tg-turing/dist/css/default/tg-turing.min.css';
+Vue.use(tgTuring);
+window["tg-turing"] = tgTuring;
+
+import tgTuringIview from 'tg-turing-iview';
+Vue.use(tgTuringIview);
+
+Object.assign(window["tg-turing"].iview.form, {
+    "text": "iview-fc-text",
+    "date": "iview-fc-date",
+    "date-local": "iview-fc-date",
+});
+
+
+import home from './pages/home.vue';
+
+
+
+Vue.use(VueRouter)
+
+if(process.env.NODE_ENV === 'development'){
+    window.apiPath = "http://127.0.0.1:7001";
+}else{
+    window.apiPath = ".";
+}
+
+const routes = [
+    { path: '/', component: home, name:"首页" },
+]
+const router = new VueRouter({
+    routes,
+})
+
+  
+new Vue({
+    el:"#root",
+    render:h=>h(App),
+    router
+})
---
+++ src/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <tg-form :fields="fields" type="iview" :column=4></tg-form>
+    </div>
+</template>
+
+<script>
+import Student from "../models/Student";
+let inst = new Student();
+export default {
+    data(){
+        return {
+            fields: inst.view("默认表单:form"),
+            param:{
+                append:"11"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
