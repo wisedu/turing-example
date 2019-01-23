34f42ffead4c44de15735ec52186d60c213c5deb
guduziye <05123875lgh@163.com>
2019-01-23 18:44:36 +0800

    更新依赖

--- controller/check-is-two-vue-include.js
+++ controller/check-is-two-vue-include.js
@@ -5,5 +5,5 @@ let vueVersions = file.match(/Vue\.version\s?\=\s?\'?\"?([^\'|\"]+)/g);
 if(!vueVersions){
     console.log('\033[31m error vue未被打包 \033[0m');
 }else if(vueVersions.length > 1){
-    console.log('\033[31m error vue被重复打包打包，\n 可能tg-turing-antd与turing-example中的vue版本不一致，\n 请升级vue \033[0m');
+    console.log('\033[31m error vue被重复打包打包，\n 可能tg-turing-antd与turing-example中的vue版本不一致，\n 存在的个版本'+vueVersions.join(' ， ')+' \n 请升级vue \033[0m');
 }
