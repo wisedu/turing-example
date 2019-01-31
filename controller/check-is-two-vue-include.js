c5e89a7a6a432044033cf42d5abeeee2d9857fbd
guduziye <05123875lgh@163.com>
2019-01-31 13:34:20 +0800

    turing打包判断

--- controller/check-is-two-vue-include.js
+++ controller/check-is-two-vue-include.js
@@ -5,5 +5,15 @@ let vueVersions = file.match(/Vue\.version\s?\=\s?\'?\"?([^\'|\"]+)/g);
 if(!vueVersions){
     console.log('\033[31m error vue未被打包 \033[0m');
 }else if(vueVersions.length > 1){
-    console.log('\033[31m error vue被重复打包打包，\n 可能tg-turing-antd与turing-example中的vue版本不一致，\n 存在的个版本'+vueVersions.join(' ， ')+' \n 请升级vue \033[0m');
+    console.log('\033[31m error wisedu-vue.pc.min.js 的 vue被重复打包打包，\n 可能tg-turing-antd与turing-example中的vue版本不一致，\n 存在的个版本'+vueVersions.join(' ， ')+' \n 请升级example的vue \033[0m');
 }
+
+
+let mobileFile = fs.readFileSync('./dist/wisedu-vue.mobile.min.js', {encoding: 'utf-8'});
+let mobileVueVersions = mobileFile.match(/Vue\.version\s?\=\s?\'?\"?([^\'|\"]+)/g);
+if(!mobileVueVersions){
+    console.log('\033[31m error vue未被打包 \033[0m');
+}else if(mobileVueVersions.length > 1){
+    console.log('\033[31m error wisedu-vue.mobile.min.js 的 vue被重复打包打包，\n 可能tg-form-didi与turing-example中的vue版本不一致，\n 存在的个版本'+vueVersions.join(' ， ')+' \n 请升级example的vue \033[0m');
+}
+
--- controller/check-tg-version.js
+++ controller/check-tg-version.js
@@ -10,11 +10,27 @@ let tgTuringAntdPackage = fs.readFileSync('./node_modules/tg-turing-antd/package
 let tgTuringAntdPackageJson = JSON.parse(tgTuringAntdPackage);
 let tgTuringVersionInAntd = tgTuringAntdPackageJson.dependencies['tg-turing'];
 tgTuringVersionInAntd = tgTuringVersionInAntd.replace(/^\^|\~/, '');
-if(tgTuringVersion !== tgTuringVersionInAntd){
-    console.log('\033[31m error tg-turing与tg-turing-antd中引用的tg-turing版本不一致 \033[0m');
+
+let tgFormDidiPackage = fs.readFileSync('./node_modules/tg-form-didi/package.json', {encoding: 'utf-8'});
+let tgFormDidiPackageJson = JSON.parse(tgFormDidiPackage);
+let tgTuringVersionInFormDidi = tgFormDidiPackageJson.dependencies['tg-turing'];
+tgTuringVersionInFormDidi = tgTuringVersionInFormDidi.replace(/^\^|\~/, '');
+
+
+if(tgTuringVersion !== tgTuringVersionInAntd || tgTuringVersion !== tgTuringVersionInFormDidi){
+    if(tgTuringVersion !== tgTuringVersionInFormDidi && tgTuringVersion !== tgTuringVersionInAntd){
+        console.log('\033[31m error tg-turing与tg-turing-antd和tg-form-didi中引用的tg-turing版本不一致 \033[0m');
+        console.log('\033[31m 请重新发布tg-turing-antd和tg-form-didi 到npm \033[0m');
+    }else if(tgTuringVersion !== tgTuringVersionInAntd){
+        console.log('\033[31m error tg-turing与tg-turing-antd中引用的tg-turing版本不一致 \033[0m');
+        console.log('\033[31m 请重新发布tg-turing-antd 到npm \033[0m');
+    }else if(tgTuringVersion !== tgTuringVersionInFormDidi){
+        console.log('\033[31m error tg-turing与tg-form-didi中引用的tg-turing版本不一致 \033[0m');
+        console.log('\033[31m 请重新发布tg-form-didi 到npm \033[0m');
+    }
+
     console.log('特意用此中断程序执行' + aaa + bbb);
-}else{
-    console.log(`tg-turing与tg-turing-antd中引用的tg-turing版本相同`);
 }
+
 
 
